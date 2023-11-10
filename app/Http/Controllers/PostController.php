11274dcda4b3db4be2ba047def66eac0a2415111
rsmirsky <Smirsky@ukr.net>
2023-11-10 12:04:30 +0200

    install inertia-vue3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+
+        return inertia('Posts/Index', compact('posts'));
+    }
 }
