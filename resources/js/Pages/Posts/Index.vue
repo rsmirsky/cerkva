b4d8a51b825298879bbb503cbe082ec10973ef3f
rsmirsky <Smirsky@ukr.net>
2023-11-12 13:17:23 +0200

    add calendar component

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,21 +1,54 @@
-<template>
-        <table class="mt-4 min-w-full divide-y divide-gray-200 border">
-
-            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-            <tr v-for="post in posts">
-                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                    {{ post.id }}
-                </td>
-            </tr>
-            </tbody>
-        </table>
-</template>
-
 <script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
 export default {
-    props: {
-        posts: Object
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [ dayGridPlugin, interactionPlugin ],
+                initialView: 'dayGridMonth',
+                dateClick: this.handleDateClick,
+                events: [
+                    { title: 'event 1', date: '2019-04-01' },
+                    { title: 'event 2', date: '2019-04-02' }
+                ]
+            }
+        }
+    },
+    methods: {
+        handleDateClick: function(arg) {
+            alert('date click! ' + arg.dateStr)
+        }
     }
 }
 </script>
+<template>
+    <FullCalendar :options="calendarOptions" />
+</template>
+
+<!--<template>-->
+<!--        <table class="mt-4 min-w-full divide-y divide-gray-200 border">-->
+
+<!--            <tbody class="bg-white divide-y divide-gray-200 divide-solid">-->
+<!--            <tr v-for="post in posts">-->
+<!--                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">-->
+<!--                    {{ post.id }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--    props: {-->
+<!--        posts: Object-->
+<!--    }-->
+<!--}-->
+<!--</script>-->
 
