afc94250289f34b40d1700fdf8bde953c96ede1d
rsmirsky <Smirsky@ukr.net>
2023-11-14 13:24:32 +0200

    include bootstrap 5 themes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,15 @@ class PostController extends Controller
 {
     public function index()
     {
+      /*  $holidays = [];
         $posts = Post::all();
+        foreach ($posts as $post){
+            $holidays[] = [
+                'title' => $post->title,
+            ];
+        }*/
+        $posts = Post::all();
+
         return inertia('Posts/Index', compact('posts'));
     }
 }
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,33 +2,36 @@
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import {formToJSON} from "axios";
 
 export default {
+    props: {
+        posts: Object,
+    },
     components: {
         FullCalendar // make the <FullCalendar> tag available
     },
     data() {
         return {
             calendarOptions: {
-                plugins: [ dayGridPlugin, interactionPlugin ],
+                plugins: [dayGridPlugin, interactionPlugin],
+                themeSystem: 'bootstrap5',
                 initialView: 'dayGridMonth',
                 dateClick: this.handleDateClick,
-                events: [
-                    { title: 'event 1', date: '2019-04-01' },
-                    { title: 'event 2', date: '2019-04-02' }
-                ]
+                events: this.posts
             }
         }
     },
     methods: {
-        handleDateClick: function(arg) {
+        handleDateClick: function (arg) {
             alert('date click! ' + arg.dateStr)
-        }
+        },
     }
 }
 </script>
+
 <template>
-    <FullCalendar :options="calendarOptions" />
+    <FullCalendar :options="calendarOptions"/>
 </template>
 
 <!--<template>-->
